18102fbaed89944b3e85305799997070c81a6c95
Lillian Rose <me@lillianro.se>
2024-09-08 15:19:55 -0400

    feat: add derive export to main crate via feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ pub use lbytes::{BytesReadExt, BytesWriteExt};
 pub mod impls;
 pub mod varint;
 
+#[cfg(feature = "derive")]
+pub use packet_binser_derive as derive;
+
 pub trait PacketSerde: Sized {
 	fn serialize<B: BytesWriteExt>(&self, buffer: &mut B) -> Result<(), lbytes::Error>;
 	fn deserialize<B: BytesReadExt>(buffer: &mut B) -> Result<Self, lbytes::Error>;
