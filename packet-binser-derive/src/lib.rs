98deb8cd37981670c5fc05177e9d7f9f71e84783
Lillian Rose <me@lillianro.se>
2024-10-29 02:25:03 -0400

    remove packet derive macro

--- packet-binser-derive/src/lib.rs
+++ packet-binser-derive/src/lib.rs
@@ -4,10 +4,8 @@
 #![allow(clippy::too_many_lines)]
 
 mod binser;
-mod packet;
 
 use binser::impl_binser;
-use packet::impl_packet;
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
@@ -15,15 +13,6 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn binser(input: TokenStream) -> TokenStream {
 	let input = parse_macro_input!(input as DeriveInput);
 	match impl_binser(&input) {
-		Ok(tokens) => tokens,
-		Err(err) => err.into_compile_error().into(),
-	}
-}
-
-#[proc_macro_derive(Packet, attributes(header))]
-pub fn packet(input: TokenStream) -> TokenStream {
-	let input = parse_macro_input!(input as DeriveInput);
-	match impl_packet(&input) {
 		Ok(tokens) => tokens,
 		Err(err) => err.into_compile_error().into(),
 	}
--- packet-binser-derive/tests/compiles.rs
+++ packet-binser-derive/tests/compiles.rs
@@ -1,18 +1,8 @@
-use packet_binser_derive::{Binser, Packet};
-
-#[derive(Packet)]
-#[header = 0x1]
-struct HandshakePacket(u8);
+use packet_binser_derive::Binser;
 
 #[derive(Binser)]
 struct MeowData {
 	message: String,
-}
-
-#[derive(Packet)]
-#[header = 0x2]
-struct MeowPacket {
-	data: MeowData,
 }
 
 #[derive(Binser)]
